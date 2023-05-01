e490ec13ff95430a128db500a9c894b01bfeee04
Vaisakh K M <68694876+Vaisakhkm2625@users.noreply.github.com>
2023-05-02 02:08:32 +0530

    Update telekasten.lua
    fixing true false

--- lua/telekasten.lua
+++ lua/telekasten.lua
@@ -405,7 +405,7 @@ local function imgFromClipboard()
     print('get_paste_command: ' .. get_paste_command(pngdir, pngname))
     local result = os.execute(get_paste_command(pngdir, pngname))
      -- print('result: ' .. result)
-    if result then
+    if not result then
         vim.api.nvim_err_writeln(
             string.format(
                 "Unable to write image %s (exit code: ).  Is there an image on the clipboard? ",
