cecae796cd763ff52b843d9dde06beb1768028f9
Vaisakh K M <68694876+Vaisakhkm2625@users.noreply.github.com>
2023-05-02 01:08:30 +0530

    Update telekasten.lua
    somthing

--- lua/telekasten.lua
+++ lua/telekasten.lua
@@ -403,8 +403,8 @@ local function imgFromClipboard()
     local relpath = make_relative_path(vim.fn.expand("%:p"), png, "/")
 
     print('get_paste_command: ' .. get_paste_command(pngdir, pngname))
-    result = os.execute(get_paste_command(pngdir, pngname))
-    print('restul: ' .. result)
+    local result = os.execute(get_paste_command(pngdir, pngname))
+     -- print('restul: ' .. result)
     if result > 0 then
         vim.api.nvim_err_writeln(
             string.format(
